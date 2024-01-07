c18574d8f7363307a43b5d0d7ee10cce9b57939f
amari <junior_maza16@hotmail.com>
2024-01-07 18:37:04 -0300

    - Extração do id no Post Booking - Criação do Get Booking

--- src/test/java/apiTest/TesteBooker.java
+++ src/test/java/apiTest/TesteBooker.java
@@ -17,16 +17,20 @@ import static org.hamcrest.Matchers.*;
 // Classe
 public class TesteBooker {     // inicio da classe
     // Atributos
+    private String bookingId;  // Agora é um campo da classe
 
     // Funções e Métodos
     // Funções de Apoio
     public static String lerArquivoJson(String arquivoJson) throws IOException {
         return new String(Files.readAllBytes(Paths.get(arquivoJson)));
+
+
     }
     @Test
-    public void testarGerarToken() throws IOException {
+    public void testarGerarToken() throws IOException { // inicio gerar Token Post
         // carregar os dados do json
         String jsonBody = lerArquivoJson("src/test/resources/json/token1.json");
+
 
         // realizar o teste
         Response response = (Response) given()
@@ -46,15 +50,15 @@ public class TesteBooker {     // inicio da classe
         String token = response.jsonPath().getString("token").substring(0);
         System.out.println("Conteúdo do Token: " + token);
 
+    }   // fim do post token
 
-    }
     @Test
-    public void testarCreateBooking() throws IOException {
+    public void testarCreateBooking() throws IOException {  // inicio do post booking
         // carregar os dados do json
         String jsonBody = lerArquivoJson("src/test/resources/json/create1.json");
 
         //realizar teste
-        given()
+       Response response = (Response) given()
                 .contentType("application/json")
                 .log().all()
                 .body(jsonBody)
@@ -67,10 +71,47 @@ public class TesteBooker {     // inicio da classe
                 .body("booking.totalprice", is(400))
                 .body("booking.depositpaid", is(true))
                 .body("booking.bookingdates.checkin", is("2024-01-01"))
-
+       .extract()
         ;
+        // Extração de ID Booking
 
-    }
+        bookingId = response.jsonPath().getString("bookingid");
+        System.out.println("Conteúdo do ID: " + bookingId );
+
+    }   // fim do post booking
+
+    @Test
+    public void testarConsultarBooking() throws IOException {   // inicio do get booking
+
+    testarCreateBooking();  // Chamando o método que cria o booking
+
+    // resultado esperados
+    String firstname = "Ronyy";
+    String lastname = "Brown";
+    int totalprice = 400;
+    boolean depositpaid = true;
+    String checkin = "2024-01-01";
+    String checkout = "2024-01-01";
+
+    given()
+            .contentType("application/json")
+            .log().all()
+    .when()
+            .get("https://restful-booker.herokuapp.com/booking/" + bookingId )
+    .then()
+            .log().all()
+            //.statusCode(200)
+            .body("firstname", is(firstname))
+            .body("lastname", is(lastname))
+            .body("totalprice", is(totalprice))
+            .body("depositpaid", is(depositpaid))
+            .body("bookingdates.checkin", is(checkin))
+            .body("bookingdates.checkout", is(checkout))
+
+    ;
+
+
+    }   // fim do get booking
 
 
 
